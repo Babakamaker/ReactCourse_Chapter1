222b6769fd415822f3950d0ac39b5561ca607b1f
Viktoriia Babaieva <viktoriia.babaieva@oa.edu.ua>
2024-09-29 18:24:21 +0300

    Completed Lab6

---
+++ Lab6/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import './App.css'
+import PageTitle from './components/PageTitle'
+import ToDosManager from './components/ToDosManager'
+
+function App() {
+  return (
+    <>
+      <PageTitle title="ToDoApp" />
+      <ToDosManager />
+    </>
+  )
+}
+
+export default App
---
+++ Lab6/src/components/PageTitle.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+const PageTitle = ({title}) => {
+    return <h1>{title}</h1>
+}
+
+export default PageTitle;
